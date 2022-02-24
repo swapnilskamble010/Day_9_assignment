6654c00bb90897f3c48293df338b1d8097cfc80d
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-02-24 16:42:33 +0530

    Add: Logic for Attendance checking for Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,14 @@
+#disply welcome message
 
 echo "Welcome to Employee Wage Computation Program"
+
+#Check Employee is Present or Absent
+
+A=$(( RANDOM%2 ))
+if (( $A==0 ))
+then
+	echo "Employee is Absent"
+else
+	echo "Employee is Present"
+fi
+
